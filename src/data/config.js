0f459131e3de360d848213174602fa02ea4deb19
Zohaib <muhammadzohaib4042@gmail.com>
2026-09-19 14:19:16 +0500

    Update Google Maps URLs in config.js

--- src/data/config.js
+++ src/data/config.js
@@ -4,7 +4,7 @@ export const PHONE_NUMBER = '0321-4145344'
 export const WHATSAPP_NUMBER = '923214145344' // digits only with country code, e.g. 923001234567
 export const ADDRESS = 'Baba Printing Point near Ali Mobile Center Kahna Nau Lahore'
 export const BUSINESS_HOURS = '12 Hours'
-export const GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/pH8bG1chE6h8Najq7' // link to open in Google Maps
-export const GOOGLE_MAPS_EMBED_URL = '<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3357.8408924643068!2d74.36349807560546!3d31.369079674283533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDIyJzA4LjciTiA3NMKwMjEnNTcuOSJF!5e1!3m2!1sen!2s!4v1789808030256!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>' // paste the src from Google Maps > Share > Embed a map
+export const GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/BfTGXzXNVeqX75pr7' // link to open in Google Maps
+export const GOOGLE_MAPS_EMBED_URL = '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d505.1398440932534!2d74.3655713!3d31.3692012!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919a959987997ff%3A0x54fb20b1425d566f!2sBABA%20PRINTING%20POINT!5e1!3m2!1sen!2s!4v1789809516073!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>' // paste the src from Google Maps > Share > Embed a map
 export const SOCIAL_LINKS = { label:'Facebook', url:'https://www.facebook.com/share/19Qhsz8PQg/' } // e.g. 
 //
