92b940e1b9e68e5b18299c46e365af187d5ede90
Zohaib <muhammadzohaib4042@gmail.com>
2026-09-19 14:06:41 +0500

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -5,6 +5,6 @@ export const WHATSAPP_NUMBER = '923214145344' // digits only with country code, 
 export const ADDRESS = ''
 export const BUSINESS_HOURS = '12 Hours'
 export const GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/pH8bG1chE6h8Najq7' // link to open in Google Maps
-export const GOOGLE_MAPS_EMBED_URL = '' // paste the src from Google Maps > Share > Embed a map
-export const SOCIAL_LINKS = [] // e.g. { label:'Facebook', url:'https://...' }
-//<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3357.8408924643068!2d74.36349807560546!3d31.369079674283533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDIyJzA4LjciTiA3NMKwMjEnNTcuOSJF!5e1!3m2!1sen!2s!4v1789808030256!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+export const GOOGLE_MAPS_EMBED_URL = '<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3357.8408924643068!2d74.36349807560546!3d31.369079674283533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDIyJzA4LjciTiA3NMKwMjEnNTcuOSJF!5e1!3m2!1sen!2s!4v1789808030256!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>' // paste the src from Google Maps > Share > Embed a map
+export const SOCIAL_LINKS = { label:'Facebook', url:'https://www.facebook.com/share/19Qhsz8PQg/' } // e.g. 
+//
