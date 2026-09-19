1e50b0fc2b31c7bc638648b6ae7daf9976f72764
Zohaib <muhammadzohaib4042@gmail.com>
2026-09-19 14:07:52 +0500

    Update business address in config.js

--- src/data/config.js
+++ src/data/config.js
@@ -2,7 +2,7 @@
 export const BUSINESS_NAME = 'BABA PRINTING POINT'
 export const PHONE_NUMBER = '0321-4145344'
 export const WHATSAPP_NUMBER = '923214145344' // digits only with country code, e.g. 923001234567
-export const ADDRESS = ''
+export const ADDRESS = 'Baba Printing Point near Ali Mobile Center Kahna Nau Lahore'
 export const BUSINESS_HOURS = '12 Hours'
 export const GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/pH8bG1chE6h8Najq7' // link to open in Google Maps
 export const GOOGLE_MAPS_EMBED_URL = '<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3357.8408924643068!2d74.36349807560546!3d31.369079674283533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDIyJzA4LjciTiA3NMKwMjEnNTcuOSJF!5e1!3m2!1sen!2s!4v1789808030256!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>' // paste the src from Google Maps > Share > Embed a map
