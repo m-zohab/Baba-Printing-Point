254067931323ee83812e2687a901fa23ddcdcca5
Zohaib <muhammadzohaib4042@gmail.com>
2026-09-19 14:29:21 +0500

    Change SOCIAL_LINKS to an array format

--- src/data/config.js
+++ src/data/config.js
@@ -6,5 +6,5 @@ export const ADDRESS = 'Baba Printing Point near Ali Mobile Center Kahna Nau Lah
 export const BUSINESS_HOURS = '12 Hours'
 export const GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/BfTGXzXNVeqX75pr7' // link to open in Google Maps
 export const GOOGLE_MAPS_EMBED_URL = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d505.1398440932534!2d74.3655713!3d31.3692012!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919a959987997ff%3A0x54fb20b1425d566f!2sBABA%20PRINTING%20POINT!5e1!3m2!1sen!2s!4v1789809516073!5m2!1sen!2s' // paste the src from Google Maps > Share > Embed a map
-export const SOCIAL_LINKS = { label:'Facebook', url:'https://www.facebook.com/share/19Qhsz8PQg/' } // e.g. 
+export const SOCIAL_LINKS = [{ label:'Facebook', url:'https://www.facebook.com/share/19Qhsz8PQg/' }] // e.g. 
 //
